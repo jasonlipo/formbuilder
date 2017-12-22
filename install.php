11e8180199a24a1cbc8e3ca086b452d53370ce98
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2017-12-22 09:55:23 +0000

    Set up project

---
+++ install.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * PHP and JS Form Builder
+ * Installation script
+ * @author Jason Lipowicz
+ * @version 0.0.1
+ */
+?>
---
+++ schema.sql
@@ -0,0 +1,111 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `formbuilder`
+--
+CREATE DATABASE formbuilder;
+USE formbuilder;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elements`
+--
+CREATE TABLE `elements` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forms`
+--
+CREATE TABLE `forms` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_elements`
+--
+CREATE TABLE `form_elements` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `form_id` int(11) NOT NULL,
+  `element_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_submissions`
+--
+CREATE TABLE `form_submissions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `form_id` int(11) NOT NULL,
+  `data` json NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for table `elements`
+--
+ALTER TABLE `elements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `forms`
+--
+ALTER TABLE `forms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `form_elements`
+--
+ALTER TABLE `form_elements`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `form_elements_form_id_element_id_index` (`form_id`,`element_id`);
+
+--
+-- Indexes for table `form_submissions`
+--
+ALTER TABLE `form_submissions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `form_submissions_form_id_index` (`form_id`);
+
+--
+-- AUTO_INCREMENT for table `elements`
+--
+ALTER TABLE `elements`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `forms`
+--
+ALTER TABLE `forms`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `form_elements`
+--
+ALTER TABLE `form_elements`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `form_submissions`
+--
+ALTER TABLE `form_submissions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
