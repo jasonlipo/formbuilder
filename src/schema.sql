3b4eda7df4f069725217cc793f1e65a4c4b552df
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2017-12-24 22:05:22 +0000

    Simplify database schema

--- src/schema.sql
+++ src/schema.sql
@@ -15,97 +15,18 @@ USE formbuilder;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `elements`
---
-CREATE TABLE `elements` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NULL DEFAULT NULL,
-  `updated_at` datetime NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `forms`
 --
 CREATE TABLE `forms` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `structure` json NOT NULL,
   `created_at` datetime NULL DEFAULT NULL,
   `updated_at` datetime NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `form_elements`
---
-CREATE TABLE `form_elements` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `form_id` int(11) NOT NULL,
-  `element_id` int(11) NOT NULL,
-  `created_at` datetime NULL DEFAULT NULL,
-  `updated_at` datetime NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `form_submissions`
---
-CREATE TABLE `form_submissions` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `form_id` int(11) NOT NULL,
-  `data` json NOT NULL,
-  `created_at` datetime NULL DEFAULT NULL,
-  `updated_at` datetime NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Indexes for table `elements`
---
-ALTER TABLE `elements`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `forms`
 --
 ALTER TABLE `forms`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `form_elements`
---
-ALTER TABLE `form_elements`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `form_elements_form_id_element_id_index` (`form_id`,`element_id`);
-
---
--- Indexes for table `form_submissions`
---
-ALTER TABLE `form_submissions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `form_submissions_form_id_index` (`form_id`);
-
---
--- AUTO_INCREMENT for table `elements`
---
-ALTER TABLE `elements`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `forms`
---
-ALTER TABLE `forms`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `form_elements`
---
-ALTER TABLE `form_elements`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `form_submissions`
---
-ALTER TABLE `form_submissions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
