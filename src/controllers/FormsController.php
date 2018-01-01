78bfeae34d56b68df5563482149b5cad5d6226ed
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2018-01-01 15:37:17 +0000

    Display repeated information in table

--- src/controllers/FormsController.php
+++ src/controllers/FormsController.php
@@ -46,31 +46,71 @@ class FormsController extends Controller {
       $structure = json_decode($f->structure, false);
 
       // Get column headers
-      $headers = [];
-      array_walk($structure->elements, function ($element, $key) use (&$headers) {
-        if (property_exists($element->props, "label") && property_exists($element->props, "id")) {
-          if (property_exists($element->props, "validation") && $element->props->validation->type == 4) {
-            // First Name fields
-            $headers[] = [$element->props->label . " (First)", $element->props->id . "_0"];
-            $headers[] = [$element->props->label . " (Last)", $element->props->id . "_1"];
-          }
-          else {
-            $headers[] = [$element->props->label, $element->props->id];
-          }
-        }
-      });
+      $headers = $this->walk_elements($structure->elements);
       
       // Get row data
       $rows = [];
       foreach ($f->submissions as $key => $response) {
-        $row = [$key+1, $response->created_at->format("d M Y H:i:s")];
-        $response_data = json_decode($response->data, false);
-        for ($i=0; $i<count($headers); $i++) {
-          $row[] = $response_data->{$headers[$i][1]};
-        }
-        $rows[] = $row;
-      }
+        // Store the repeat count
+        $repeat_count = 0;
 
+        $this_row = [
+          'standard' => [],
+          'repeats' => []
+        ];
+        $response_data = json_decode($response->data, true);
+        for ($i=0; $i<count($headers); $i++) {
+          if ($headers[$i][2]) {
+            // We're dealing with a repeated field
+            // so use regular expressions
+            $this_field = [];
+            foreach (array_keys($response_data) as $k) {
+              if (preg_match("/^(".$headers[$i][1] . ")$/", $k, $matches)) {
+                $this_field[] = $response_data[$matches[1]];
+              }
+            }
+            $this_row['standard'][$i] = $this_field;
+            $repeat_count = max($repeat_count, count($this_field));
+          }
+          else {
+            $value = $response_data[$headers[$i][1]];
+            if (is_array($value)) {
+              $value = implode(", ", $value);
+            }
+            $this_row['standard'][$i] = $value;
+          }
+        }
+
+        // For all repeaters, leave the first one and extract the rest
+        for ($i=0; $i<count($this_row['standard']); $i++) {
+          if (is_array($this_row['standard'][$i])) {
+            $first = array_shift($this_row['standard'][$i]);
+            $this_row['repeats'][] = $this_row['standard'][$i];
+            $this_row['standard'][$i] = [$first, 0];
+          }
+        }
+
+        // Convert the repeats array into columns
+        $temp_repeats = $this_row['repeats'];
+        $num_repeats = count(current($temp_repeats));
+        $this_row['repeats'] = [];
+        for ($i=0; $i<$num_repeats; $i++) {
+          $this_row['repeats'][$i] = array_column($temp_repeats, $i);
+        }
+
+        // Add the response count and timestamps
+        array_unshift($this_row['standard'], $key+1, $response->created_at->format("d M Y H:i:s"));
+
+        // Set the row span of the standard columns
+        for ($i=0; $i<count($this_row['standard']); $i++) {
+          if (!is_array($this_row['standard'][$i])) {
+            $this_row['standard'][$i] = [$this_row['standard'][$i], $repeat_count];
+          }
+        }
+
+        $rows[] = $this_row;
+      }
+    
       // Reformat column array
       $headers = array_merge(["Response", "Submission Date"], array_map(function($col) {
         return $col[0];
@@ -86,6 +126,27 @@ class FormsController extends Controller {
     catch (ActiveRecord\RecordNotFound $e) {
       header("Location: /");
     }
+  }
+
+  private function walk_elements($arr, $repeater=false) {
+    $result = [];
+    array_walk($arr, function ($element, $key) use (&$result, $repeater) {
+      if (property_exists($element->props, "validation") &&
+          property_exists($element->props->validation, "type") &&
+          $element->props->validation->type == 4) {
+        // First Name fields
+        $result[] = [$element->props->label . " (First)", $element->props->id . ($repeater?"_([0-9]+)":"") . "_0", $repeater];
+        $result[] = [$element->props->label . " (Last)", $element->props->id . ($repeater?"_([0-9]+)":"") . "_1", $repeater];
+      }
+      else if (property_exists($element->props, "children")) {
+        // Repeater field
+        $result = array_merge($result, $this->walk_elements($element->props->children, true));
+      }
+      else if (property_exists($element->props, "label") && property_exists($element->props, "id")) {
+        $result[] = [$element->props->label, $element->props->id . ($repeater?"_([0-9]+)":""), $repeater];
+      }
+    });
+    return $result;
   }
 
   public function delete($formId) {
