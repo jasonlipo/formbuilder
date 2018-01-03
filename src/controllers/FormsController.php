7ab5cbd2102f9519a7d7c7b115e94327aaff1da9
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2018-01-03 11:51:25 +0000

    Stripe payment taken

--- src/controllers/FormsController.php
+++ src/controllers/FormsController.php
@@ -186,6 +186,49 @@ class FormsController extends Controller {
   }
 
   public function stripe($formId, $responseIdEnc) {
+    $form = Form::find($formId);
+    $form_structure = json_decode($form->structure, false);
+    $stripe_sk_key = $form_structure->props->stripe_secret_key;
+    
+    $submission = Submission::find($this->decrypt_string($responseIdEnc));
+    $submit_data = json_decode($submission->data, false);
+    $total_price = $submit_data->total_price;
+    
+    require_once "../vendor/stripe-php-5.8.0/init.php";
+    \Stripe\Stripe::setApiKey($stripe_sk_key);
+    $token = $_POST['stripeToken'];
+
+    $customer = \Stripe\Customer::create(array(
+      "source" => $token,
+    ));
+    
+    try {
+      $charge = \Stripe\Charge::create(array(
+        "amount" => floatval($total_price)*100,
+        "currency" => "gbp",
+        "description" => $form->name,
+        "metadata" => ["form_id" => $form->id, "submission_id" => $submission->id],
+        "customer" => $customer->id
+      ));
+    }
+    catch (Stripe_CardError $e) {
+      $submit_data->payment_status = "declined";
+    }
+    catch (Exception $e) {
+      $submit_data->payment_status = "unknown";
+    }
+
+    if ($charge->paid === true) {
+      $submit_data->payment_status = "paid";
+      $submit_data->payment_date = date("Y-m-d H:i:s");
+      $submit_data->payment_currency = $charge->currency;
+    }
+
+    $submission->data = json_encode($submit_data);
+    $submission->save();
+
+    header("Location: " . $form_structure->props->redirect);
+
   }
 
   public function submit($formId) {
