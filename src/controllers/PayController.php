9221325a5d821f4a254de4bd92288c56a206e3e0
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2018-01-04 22:35:19 +0000

    Clean up PayController

--- src/controllers/PayController.php
+++ src/controllers/PayController.php
@@ -7,29 +7,15 @@ class PayController extends Controller {
   public function update($formId, $responseIdEnc) {
     $form = Form::find($formId);
     $form_structure = $form->structure();
-    $stripe_sk_key = $form_structure->props->stripe_secret_key;
     
     $submission = Submission::from_encrypted($responseIdEnc);
     $submit_data = json_decode($submission->data, false);
     $total_price = $submit_data->total_price;
-    
-    require_once "../vendor/stripe-php-5.8.0/init.php";
-    \Stripe\Stripe::setApiKey($stripe_sk_key);
-    $token = $_POST['stripeToken'];
+    $this->stripe_init($form_structure);
 
     try {
-      $customer = \Stripe\Customer::create(array(
-        "source" => $token,
-        "description" => $_POST['formpay-cardholder']
-      ));
-
-      $charge = \Stripe\Charge::create(array(
-        "amount" => floatval($total_price)*100,
-        "currency" => "gbp",
-        "description" => $form->name,
-        "metadata" => ["form_id" => $form->id, "submission_id" => $submission->id],
-        "customer" => $customer->id
-      ));
+      $customer = $this->stripe_create_customer($_POST['stripeToken'], $_POST['formpay-cardholder']);
+      $charge = $this->stripe_create_charge($form, $submission, $customer, $total_price);
     }
     catch (\Stripe\Error\Card $e) {
       $submit_data->payment_status = "declined";
@@ -51,7 +37,29 @@ class PayController extends Controller {
     $submission->save();
 
     header("Location: " . $form_structure->props->redirect);
+  }
+  
+  private function stripe_init($form_structure) {
+    $stripe_sk_key = $form_structure->props->stripe_secret_key;
+    require_once "../vendor/stripe-php-5.8.0/init.php";
+    \Stripe\Stripe::setApiKey($stripe_sk_key);
+  }
 
+  private function stripe_create_customer($token, $cardholder) {
+    return \Stripe\Customer::create(array(
+      "source" => $token,
+      "description" => $cardholder
+    ));
+  }
+
+  private function stripe_create_charge($form, $submission, $customer, $price) {
+    return \Stripe\Charge::create(array(
+      "amount" => floatval($price)*100,
+      "currency" => "gbp",
+      "description" => $form->name,
+      "metadata" => ["form_id" => $form->id, "submission_id" => $submission->id],
+      "customer" => $customer->id
+    ));
   }
 }
 ?>
