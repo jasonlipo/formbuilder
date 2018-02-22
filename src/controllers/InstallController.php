46dca80654d4a8f4a7e4b7b93db212e4e0a447eb
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-02-22 21:22:10 +0000

    Global variable required for checking installation

--- src/controllers/InstallController.php
+++ src/controllers/InstallController.php
@@ -1,6 +1,7 @@
 <?php
 class InstallController extends Controller {
   public function check() {
+    global $database_name;
     $conn = ActiveRecord\ConnectionManager::get_connection("install");
     if (!$conn->query("SHOW DATABASES LIKE '".$database_name."'")->rowCount()) {
       $this->render('need_install.html', []);
@@ -9,6 +10,7 @@ class InstallController extends Controller {
   }
 
   public function show() {
+    global $database_name;
     $conn = ActiveRecord\ConnectionManager::get_connection("install"); 
     if (!$conn->query("SHOW DATABASES LIKE '".$database_name."'")->rowCount()) {
       $conn->query(file_get_contents("../src/schema.sql"));
