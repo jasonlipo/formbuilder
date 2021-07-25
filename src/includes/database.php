aaa9ac4a3b707824968123961b0a39c402d68598
Jason Lipowicz <jason@lipowicz.com>
2021-07-25 17:14:24 +0100

    Update database.php

--- src/includes/database.php
+++ src/includes/database.php
@@ -7,8 +7,8 @@ require_once '../vendor/phpdotenv/Loader.php';
 $dotenv = new Dotenv\Dotenv(dirname(dirname(__DIR__)));
 $dotenv->load();
 
-$database_name = (isset(getenv('DATABASE_NAME'))) ? getenv('DATABASE_NAME') : "formbuilder";
-$database_host = (isset(getenv('DATABASE_HOST'))) ? getenv('DATABASE_HOST') : "127.0.0.1";
+$database_name = getenv('DATABASE_NAME') !== false ? getenv('DATABASE_NAME') : "formbuilder";
+$database_host = getenv('DATABASE_HOST') !== false ? getenv('DATABASE_HOST') : "127.0.0.1";
 
 ActiveRecord\Config::initialize(function($cfg) use ($database_name, $database_host) {
   $cfg->set_model_directory('../src/models');
