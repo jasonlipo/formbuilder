92d3356d98f8bd25303ed377b960cb93fca3806d
Jason Lipowicz <jason@lipowicz.com>
2021-07-25 17:25:27 +0100

    Update env variables (#28)
    * Update .env
    
    * Update database.php

--- src/includes/database.php
+++ src/includes/database.php
@@ -7,14 +7,14 @@ require_once '../vendor/phpdotenv/Loader.php';
 $dotenv = new Dotenv\Dotenv(dirname(dirname(__DIR__)));
 $dotenv->load();
 
-$database_name = getenv('DATABASE_NAME') !== false ? getenv('DATABASE_NAME') : "formbuilder";
-$database_host = getenv('DATABASE_HOST') !== false ? getenv('DATABASE_HOST') : "127.0.0.1";
+$database_name = getenv('FORMBUILDER_NAME') !== false ? getenv('FORMBUILDER_NAME') : "formbuilder";
+$database_host = getenv('FORMBUILDER_HOST') !== false ? getenv('FORMBUILDER_HOST') : "127.0.0.1";
 
 ActiveRecord\Config::initialize(function($cfg) use ($database_name, $database_host) {
   $cfg->set_model_directory('../src/models');
   $cfg->set_connections(array(
-    'install' => 'mysql://'.getenv('DATABASE_USER').':'.getenv('DATABASE_PASSWORD').'@'.$database_host,
-    'dev' => 'mysql://'.getenv('DATABASE_USER').':'.getenv('DATABASE_PASSWORD').'@'.$database_host.'/'.$database_name
+    'install' => 'mysql://'.getenv('FORMBUILDER_USER').':'.getenv('FORMBUILDER_PASSWORD').'@'.$database_host,
+    'dev' => 'mysql://'.getenv('FORMBUILDER_USER').':'.getenv('FORMBUILDER_PASSWORD').'@'.$database_host.'/'.$database_name
   ));
 });
 
