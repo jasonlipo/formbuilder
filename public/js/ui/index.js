2256ee9ce5828dbcc548c3d3b67d09df05a4dda4
Jason Lipowicz <jason@lipowicz.com>
2021-08-02 21:19:47 +0100

    Update index.js

--- public/js/ui/index.js
+++ public/js/ui/index.js
@@ -1,5 +1,16 @@
 var formui;
+
+function showForms() {
+  if (typeof FormLoad !== "function" || typeof FormPages !== "function" || typeof FormValidator !== "function" || 
+      typeof FormSave !== "function" || typeof FormPayment !== "function") {
+    setTimeout(showForms, 500);
+  }
+  else {
+    formui = new FormUI($('.formui-app'));
+    formui.init();
+  }
+}
+
 $(function () {
-  formui = new FormUI($('.formui-app'));
-  formui.init();
+  showForms();
 });
