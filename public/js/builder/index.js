165a2724be0b7c4f11ab37d6ed8cb0ecb557632c
Jason Lipowicz <jason@lipowicz.com>
2021-08-02 21:18:43 +0100

    Update index.js

--- public/js/builder/index.js
+++ public/js/builder/index.js
@@ -1,5 +1,16 @@
 var formbuilder;
+
+function buildForms() {
+  if (typeof FormSettings !== "function" || typeof FormSave !== "function" || typeof FormLoad !== "function" || 
+      typeof FormStructure !== "function" || typeof FormPayment !== "function" || typeof FormElementList !== "object") {
+    setTimeout(buildForms, 500);
+  }
+  else {
+    formbuilder = new FormBuilder($('.formbuilder-app'));
+    formbuilder.init();
+  }
+}
+
 $(function () {
-  formbuilder = new FormBuilder($('.formbuilder-app'));
-  formbuilder.init();
+  buildForms();
 });
